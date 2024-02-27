7f5c77be26318e5e9bea00557b8dc44656e52c21
Jystine <jystine123@gmail.com>
2024-02-26 22:13:35 -0800

    installed packages

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,5 +1,9 @@
 <script>
-  // Write your JS here, or import other files
+  import * as d3 from "d3";
+  import Scroller from "@sveltejs/svelte-scroller";
+
+  let count, index, offset, progress;
+
 </script>
 
 <main>
@@ -8,6 +12,36 @@
   <p>Write your HTML here</p>
 </main>
 
+<Scroller
+  top = {0.0}
+  bottom = {1}
+  threshold = {0.5}
+  bind:count
+  bind:index
+  bind:offset
+  bind:progress>
+
+  <div class = "foreground" slot = "foreground">
+    <section>This is the first section.</section>
+    <section>This is the second section.</section>
+    <section>This is the third section.</section>
+    <section>This is the fourth section.</section>
+    <section>This is the fifth section.</section>
+    <section>This is the sixth section.</section>
+  </div>
+
+</Scroller>
+
 <style>
-  /* Write your CSS here */
+  section {
+    height: 80vh;
+    background-color: rgba(0, 0, 0, 0.2); /* 20% opaque */
+    /* color: white; */
+    outline: magenta solid 3px;
+    text-align: center;
+    max-width: 4474px; /* adjust at will */
+    color: black;
+    padding: 1em;
+    margin: 2em 2em 2em 2em;
+  }
 </style>
